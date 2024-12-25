b1c760f8584a696860178f31145480b30d63bdd2
rizwanali <myofficeid192@gmail.com>
2024-12-25 11:23:41 +0500

    Turn off DEBUG mode in settings

--- cheezious/settings.py
+++ cheezious/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e%c%y1v-t^v^2ndvq5_65vo$3r!thrbnfg!o@8pigm&)kna)dm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
